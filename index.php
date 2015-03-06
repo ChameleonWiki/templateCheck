de717a0806ee1882710a0f3a398fb5e20854a6d2
ChameleonWiki <chameleon222.wiki@gmail.com>
2015-03-06 18:36:45 +0100

    Support for localisation

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 /*
 	Purpose:  Checks and reports which articles that transcludes a template that are not linked from the template,
 	          and which articles that are linked from the template but don't transclude the template.
-	          Typical use: http://en.wikipedia.org/wiki/Template:Squad_maintenance
+	          Doc: http://tools-static.wmflabs.org/templatetransclusioncheck/
+	          Typical usage: http://en.wikipedia.org/wiki/Template:Squad_maintenance
 	Written:  03. Jan. 2015
 
 	Copyright (c) 2015, Chameleon
@@ -30,6 +31,7 @@ define('protocol', isset($_SERVER['HTTPS']) ? 'https' : (isset($_SERVER['HTTP_X_
 define('scriptLink', './index.php');
 define('cssLink', protocol . '://tools-static.wmflabs.org/templatetransclusioncheck/main.css');
 define('docLink', protocol . '://tools-static.wmflabs.org/templatetransclusioncheck/');
+define('i18nDomain', 'templatetransclusioncheck');
 
 // Dependencies
 require_once('./dependencies/database.php');
@@ -38,6 +40,10 @@ require_once('./dependencies/maintenanceNotice.php');
 // External dependencies
 require_once('./dependencies/Peachy/Includes/Hooks.php');
 require_once('./dependencies/Peachy/HTTP.php');
+require_once('/data/project/intuition/src/Intuition/ToolStart.php');
+
+$I18N = new Intuition(array('domain' => i18nDomain));
+//$I18N->loadTextdomainFromFile('./Templatetransclusioncheck.i18n.php', i18nDomain);
 
 function wpServer($language)
 {
@@ -61,9 +67,10 @@ function wpLink($wpServer, $title, $exists = true, $noredirect = false)
 		$url = $wpServer . 'wiki/' . $titleUrl;
 	
 	if ($exists) // Existing page?
-		return '<a href="' . $url . '">' . $titleHtml . '</a> (<a href="' . $urlEdit . '">edit</a>)';
-	return '<a class ="redlink" href="' . $url . '">' . $titleHtml . '</a> (<a href="' . $urlEdit . '">create</a>)';
+		return '<a href="' . $url . '">' . $titleHtml . '</a> ' . _g('parentheses', array('variables' => array('<a href="' . $urlEdit . '">' . _html('link-edit') . '</a>')));
+	return '<a class ="redlink" href="' . $url . '">' . $titleHtml . '</a> ' . _g('parentheses', array('variables' => array('<a href="' . $urlEdit . '">' . _html('link-create') . '</a>')));
 }
+
 
 function wpApiClient($wpServer)
 {
@@ -74,7 +81,7 @@ function wpApiClient($wpServer)
 	}
 	catch (Exception $e)
 	{
-		die('Oops, sorry: ' . $e->getMessage());
+		die($I18N->msg('error-exception') . ' ' . $e->getMessage()); // 'Caught an exception:'
 	}
 	$wpApi['url'] = $wpServer . 'w/api.php?format=json&continue=';
 	return $wpApi;
@@ -91,7 +98,7 @@ function transclusionsOf($wpApi, $template)
 	}
 	catch (Exception $e)
 	{
-		die('Oops, sorry: ' . $e->getMessage());
+		die($I18N->msg('error-exception') . ' ' . $e->getMessage()); // 'Caught an exception:'
 	}
 	if ($json == false || count($json) == 0) return array();
 	
@@ -111,7 +118,7 @@ function linksFrom($wpApi, $template)
 	}
 	catch (Exception $e)
 	{
-		die('Oops, sorry: ' . $e->getMessage());
+		die($I18N->msg('error-exception') . ' ' . $e->getMessage()); // 'Caught an exception:'
 	}
 	if($json == false || count($json) == 0) return array();
 
@@ -239,11 +246,11 @@ function arrayDiff($arrayA, $arrayB)
 }
 
 $oldTime = $_SERVER['REQUEST_TIME'] ? $_SERVER['REQUEST_TIME'] : time();
-$language = (isset($_GET['lang']) && $_GET['lang'] != '') ? htmlspecialchars($_GET['lang']) : 'no';
+$language = (isset($_GET['lang']) && $_GET['lang'] != '') ? htmlspecialchars($_GET['lang']) : explode('-', $I18N->getLang());
 $template = (isset($_GET['name']) && $_GET['name'] != '') ? str_replace('_', ' ', $_GET['name']) : '';
 $complete = (isset($_GET['complete']) && $_GET['complete'] === '1') ? true : false;
 
-if(!preg_match('/^[a-z-]{2,8}$/', $language)) die("Oops, sorry: I don't speak that language..."); // Safety precaution
+if(!preg_match('/^[a-z-]{2,8}$/', $language)) die($I18N->msg('error-language')); // Safety precaution ('Unknown language.')
 
 define('redirectSymbolR', ' <span class="redirect">&rarr;</span> ');
 define('redirectSymbolL', ' <span class="redirect">&larr;</span> ');
@@ -252,7 +259,7 @@ define('redirectSymbolL', ' <span class="redirect">&larr;</span> ');
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<title>Template linking and transclusion check</title>
+		<title><?php echo _html('title'); ?></title>
 		<link rel="stylesheet" href="<?php echo cssLink; ?>" type="text/css" />
 		<meta name="robots" content="noindex,nofollow" />
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
@@ -263,13 +270,13 @@ define('redirectSymbolL', ' <span class="redirect">&larr;</span> ');
 	<body>
 		<?php MaintenanceNotice::displayMessage(); ?>
 		<p><a href="<?php echo protocol; ?>://tools.wmflabs.org/"><img src="<?php echo protocol; ?>://upload.wikimedia.org/wikipedia/commons/b/bf/Powered-by-tool-labs.png" alt="Powered by Wikimedia Tool Labs icon" width="105" height="40" id="logo" /></a></p>
-		<h1>Template linking and transclusion check</h1>
-		<p>Checks and reports which articles that transcludes a template that are not linked from the template, and which articles that are linked from a template but don't transclude it.</p>
+		<h1><?php echo _html('title'); ?></h1>
+		<p><?php echo _html('description'); ?></p>
 		<form action="<?php echo scriptLink; ?>" method="get">
 			<table>
-				<tr><td><label for="lang">Language:</label></td><td><input type="text" name="lang" id="lang" value="<?php echo $language; ?>" style="width:80px;" maxlength="8" required="required" />.wikipedia.org</td></tr>
-				<tr><td><label for="name">Template name:</label></td><td><input type="text" name="name" id="name" value="<?php echo htmlspecialchars($template, ENT_QUOTES); ?>" style="width:200px;" required="required" /> (including namespace)</td></tr>
-				<tr><td><label for="complete">Generate complete report:</label></td><td><input type="checkbox" name="complete" id="complete" value="1"<?php if ($complete) echo ' checked="checked"'; ?> /><input type="submit" value="Check!" /></td></tr>
+				<tr><td><label for="lang"><?php echo _html('form-label-language'); ?></label></td><td><input type="text" name="lang" id="lang" value="<?php echo $language; ?>" style="width:80px;" maxlength="8" required="required" />.wikipedia.org</td></tr>
+				<tr><td><label for="name"><?php echo _html('form-label-template'); ?></label></td><td><input type="text" name="name" id="name" value="<?php echo htmlspecialchars($template, ENT_QUOTES); ?>" style="width:400px;" required="required" /> <?php echo _html('form-label-template2'); ?></td></tr>
+				<tr><td><label for="complete"><?php echo _html('form-label-complete'); ?></label></td><td><input type="checkbox" name="complete" id="complete" value="1"<?php if ($complete) echo ' checked="checked"'; ?> /><input type="submit" value="<?php echo _g('form-submit'); ?>" /></td></tr>
 			</table>
 		</form>
 <?php
@@ -283,28 +290,28 @@ if (isset($_GET['lang']) && $template != '')
 	}
 	catch (Exception $e)
 	{
-		die('Oops, sorry: ' . $e->getMessage());
+		die($I18N->msg('error-exception') . ' ' . $e->getMessage()); // 'Caught an exception:'
 	}
 	
 	$server = wpServer($language);
 	if (!templateExists($db, $template))
-		echo '<p>Template ' . wpLink($server, $template, false) . " does not exist.</p>\n";
+		echo '<p>' . _html('feedback-template-missing', array('variables' => array(wpLink($server, $template, false)), 'raw-variables' => true)) . "</p>\n"; // 'Template $1 does not exist.'
 	else // Seems like it exist...
 	{
-		$wpApi = wpApiClient($server) or die("Oops, sorry: Couldn't get the API client");
+		$wpApi = wpApiClient($server) or die($I18N->msg('error-api')); // "Couldn't get the API client."
 		$transclusions = transclusionsOf($wpApi, $template);
 		$links = linksFrom($wpApi, $template);
 		unset($wpApi);
 		$redirects = checkStatus($db, $links); // Check which links that do not exist		
 		
-		echo '<p>Results for ' . wpLink($server, $template) . "</p>\n";		
+		echo '<p>' . _html('feedback-result', array('variables' => array(wpLink($server, $template)), 'raw-variables' => true)) . "</p>\n"; // 'Results for $1'
 		echo '<table style="width: 90%;">' . "\n";
-		echo '<tr><th colspan="2" class="verbose">Mismatch between transclusions and links</th></tr>' . "\n";
-		echo '<tr><th style="width: 50%;">Transclusion but no link</th><th>Link but no transclusion</th></tr>' . "\n";
+		echo '<tr><th colspan="2" class="verbose">' . _html('feedback-header-mismatch') . "</th></tr>\n"; // 'Mismatch between transclusions and links'
+		echo '<tr><th style="width: 50%;">' . _html('feedback-header-no-link') . '</th><th>' . _html('feedback-header-no-transclusion') . "</th></tr>\n"; // 'Transclusion but no link', 'Link but no transclusion'
 		
 		// Any articles that transcludes template but are not linked from template?
 		$notLinked = arrayDiff($transclusions, $links);
-		echo '<tr><td><p>Total: ' . count($notLinked);
+		echo '<tr><td><p>' . _html('feedback-total-count', array('variables' => array(count($notLinked))));
 		if (!empty($notLinked))
 		{
 			foreach ($notLinked as $c)
@@ -314,7 +321,7 @@ if (isset($_GET['lang']) && $template != '')
 
 		// Any articles that are linked from the template but do not transclude the template?
 		$notTranscluding = arrayDiff($links, $transclusions);
-		echo '<td><p>Total: ' . count($notTranscluding);
+		echo '<td><p>' . _html('feedback-total-count', array('variables' => array(count($notTranscluding))));
 		if (!empty($notTranscluding))
 		{
 			foreach ($notTranscluding as $c)
@@ -330,8 +337,8 @@ if (isset($_GET['lang']) && $template != '')
 		if (!empty($redirects))
 		{
 			// Display all links to redirects from template
-			echo '<tr><th>&nbsp;</th><th>Links to redirects</th></tr>' . "\n";
-			echo '<tr><td>&nbsp;</td><td><p>Total: ' . count($redirects);
+			echo '<tr><th>&nbsp;</th><th>' . _html('feedback-header-redirects') . "</th></tr>\n"; // 'Links to redirects'
+			echo '<tr><td>&nbsp;</td><td><p>' . _html('feedback-total-count', array('variables' => array(count($redirects))));
 			foreach ($redirects as $c)
 			{
 				echo "\n<br />" . wpLink($server, $c['title'], $c['pageid'] != 0, true);
@@ -343,9 +350,9 @@ if (isset($_GET['lang']) && $template != '')
 		if ($complete) // Display 
 		{
 			// Template transclusions
-			echo '<tr><th colspan="2" class="verbose">Complete transclusion and link overview</th></tr>' . "\n";
-			echo "<tr><th>Transclutions of template</th><th>Links from template</th></tr>\n";
-			echo '<tr><td><p>Transclusion count: ' . count($transclusions);
+			echo '<tr><th colspan="2" class="verbose">' . _html('feedback-header-complete') . "</th></tr>\n"; // 'Complete transclusion and link overview'
+			echo '<tr><th>' . _html('feedback-header-transclusions') . '</th><th>' . _html('feedback-header-links') . "</th></tr>\n"; // 'Transclutions of template', 'Links from template'
+			echo '<tr><td><p>' . _html('feedback-total-count', array('variables' => array(count($transclusions))));
 			if (!empty($transclusions))
 			{
 				foreach ($transclusions as $c)
@@ -364,7 +371,7 @@ if (isset($_GET['lang']) && $template != '')
 			echo "</p></td>\n";
 
 			// Links from template to articles
-			echo '<td><p>Link count: ' . count($links);
+			echo '<td><p>' . _html('feedback-total-count', array('variables' => array(count($links))));
 			if (!empty($links))
 			{
 				foreach ($links as $c)
@@ -380,15 +387,17 @@ if (isset($_GET['lang']) && $template != '')
 		echo "</table>\n";
 	
 		if (!$complete)
-			echo '<p>&nbsp;</p><p><a href="' . scriptLink . "?lang=$language&amp;name=" . wpLinkUrlEncode($template) . '&amp;complete=1">Complete report...</a></p>' . "\n";
+			echo '<p>&nbsp;</p><p><a href="' . scriptLink . "?lang=$language&amp;name=" . wpLinkUrlEncode($template) . '&amp;complete=1">' . _html('link-complete') ."</a></p>\n";
 	}
 	unset($db); // Close db
 	$diffTime = time() - $oldTime;
-	echo '<p class="stats">Generated: ' . date('D, d M Y H:i:s T') . '. Duration: ' . $diffTime . ' s.</p>';
+	//echo '<p class="stats">Generated: ' . date('D, d M Y H:i:s T') . '. Duration: ' . $diffTime . ' s.</p>';
+	echo '<p class="stats">' . _html('footer-stats', array('variables' => array($I18N->dateFormatted('%a, %d %b %Y %T %Z'), $diffTime))) . '</p>';
 }
 ?>
 <!-- div id="w3c"><a href="http://validator.w3.org/check?uri=referer"><img src="http://www.w3.org/Icons/valid-xml11-blue.png" alt="Valid XHTML 1.1 Strict" width="88" height="31" /></a>
 <a href="http://jigsaw.w3.org/css-validator/check/referer"><img src="http://www.w3.org/Icons/valid-css-blue.png" alt="Valid CSS" width="88" height="31" /></a></div -->
-<p class="info"><a href="<?php echo docLink; ?>">Tool</a> provided by <a href="<?php echo protocol; ?>://wikitech.wikimedia.org/wiki/User:Chameleon">Chameleon</a> 2015. Powered by <a href="<?php echo protocol; ?>://tools.wmflabs.org/">Wikimedia Labs</a>.</p>
+<p class="info"><a href="<?php echo docLink; ?>">Tool</a> is provided by <a href="<?php echo protocol; ?>://wikitech.wikimedia.org/wiki/User:Chameleon">Chameleon</a> 2015. Powered by <a href="<?php echo protocol; ?>://tools.wmflabs.org/">Wikimedia Labs</a>.</p>
+<?php echo $I18N->getFooterLine(i18nDomain); ?>
 </body>
 </html>
